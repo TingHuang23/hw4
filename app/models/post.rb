27c2c8b905018397a23517a3733692b9a7a6ba83
Ting Huang <ting.huang@kellogg.northwestern.edu>
2023-05-29 22:05:52 +0000

    0529

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
 end
+
+# This is testing
+# class Post < ApplicationRecord
+#   belongs_to :place
+#   belongs_to :user
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
 end
+
+# This is testing
+# class User < ApplicationRecord
+#   has_many :posts
+#   has_many :places, through: :posts, source: :place
+# end
