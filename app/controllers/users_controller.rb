4258b130e6ff70c05820da06ee9da6d3c664a9f3
Ting Huang <ting.huang@kellogg.northwestern.edu>
2023-05-29 21:20:42 +0000

    0529

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,35 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
+    @user["username"] = params["user"]["username"] #add
     @user["email"] = params["user"]["email"]
     # @user["password"] = params["user"]["password"]
     @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/login"
   end
 end
+
+# class UsersController < ApplicationController
+#   def new
+#     @user = User.new
+#   end
+
+#   def create
+#     @user = User.new(user_params)
+    
+#     if @user.save
+#       flash[:notice] = "User created successfully. Please log in."
+#       redirect_to login_path
+#     else
+#       flash[:notice] = "Failed to create user."
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def user_params
+#     params.require(:user).permit(:username, :password)
+#   end
+# end
